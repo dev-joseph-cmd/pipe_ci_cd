21546e92296cd86720b2dfae18a3f3ea7824ab2a
Joseph Aidoo <joseph.aidoo@rancard.com>
2025-06-13 13:02:01 +0000

    my first commit

---
+++ src/test/java/com/pm/start_ci_cd/StartCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.start_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
